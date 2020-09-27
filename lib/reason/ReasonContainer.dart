6079059e7fd942b9412dec5001165092b6bce5f9
Robb <robblovell@gmail.com>
2020-09-26 21:51:20 -0700

    Fixed Reason Page Scroll

--- lib/reason/ReasonContainer.dart
+++ lib/reason/ReasonContainer.dart
@@ -8,54 +8,55 @@ class TheReason extends StatelessWidget {
     return CustomScrollView(shrinkWrap: false, slivers: <Widget>[
       SliverToBoxAdapter(
         child: Padding(
-                padding: EdgeInsets.all(20),
-              child: RichText(
-              overflow: TextOverflow.visible,
-              text: TextSpan(
-                  style: TextStyle(
-                      fontWeight: FontWeight.w300,
-                      fontSize: 20,
-                      color: Colors.black),
-                  children: <TextSpan>[
-                    // todo: clean this code up!
-                    TextSpan(
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 20,
-                          color: Colors.black,
-                        ),
-                        text: 'REASON_TITLE'.tr() + '\n\n'),
-                    TextSpan(text: 'REASON1'.tr() + '\n\n'),
-                    TextSpan(text: 'REASON2'.tr() + '\n\n'),
-                    TextSpan(text: 'REASON3'.tr() + '\n\n'),
-                    TextSpan(text: 'REASON4'.tr() + '\n\n'),
-                    TextSpan(text: 'REASON5'.tr() + '\n\n'),
-                    TextSpan(
-                        style: TextStyle(
-                          fontWeight: FontWeight.w500,
-                          fontSize: 20,
-                          color: Colors.black,
-                        ),
-                        text: 'EXPLAIN_TITLE'.tr() + '\n\n'),
-                    TextSpan(text: 'EXPLAIN1'.tr() + '\n\n'),
-                    TextSpan(text: 'EXPLAIN2'.tr() + '\n\n'),
-                    TextSpan(text: 'EXPLAIN3'.tr() + '\n\n'),
-                    TextSpan(text: 'EXPLAIN4'.tr() + '\n\n'),
-                    TextSpan(text: 'EXPLAIN5'.tr() + '\n\n'),
-                    TextSpan(text: 'EXPLAIN6'.tr() + '\n\n'),
-                    TextSpan(text: 'EXPLAIN7'.tr() + '\n\n'),
-                    TextSpan(text: 'EXPLAIN8'.tr() + '\n\n'),
-                    TextSpan(text: 'EXPLAIN9'.tr() + '\n\n'),
-                    TextSpan(text: 'EXPLAIN10'.tr() + '\n\n'),
-                    TextSpan(text: 'EXPLAIN11'.tr() + '\n\n'),
-                    TextSpan(text: 'EXPLAIN12'.tr() + '\n\n'),
-                    TextSpan(text: 'EXPLAIN13'.tr() + '\n\n'),
-                    TextSpan(text: 'EXPLAIN14'.tr() + '\n\n'),
-                    TextSpan(text: 'EXPLAIN15'.tr() + '\n\n'),
-                  ]),
-            ),),
+          padding: EdgeInsets.all(20),
+          child: RichText(
+            overflow: TextOverflow.visible,
+            text: TextSpan(
+                style: TextStyle(
+                    fontWeight: FontWeight.w300,
+                    fontSize: 20,
+                    color: Colors.black),
+                children: <TextSpan>[
+                  // todo: clean this code up!
+                  TextSpan(
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                      text: 'REASON_TITLE'.tr() + '\n\n'),
+                  TextSpan(text: 'REASON1'.tr() + '\n\n'),
+                  TextSpan(text: 'REASON2'.tr() + '\n\n'),
+                  TextSpan(text: 'REASON3'.tr() + '\n\n'),
+                  TextSpan(text: 'REASON4'.tr() + '\n\n'),
+                  TextSpan(text: 'REASON5'.tr() + '\n\n'),
+                  TextSpan(
+                      style: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 20,
+                        color: Colors.black,
+                      ),
+                      text: 'EXPLAIN_TITLE'.tr() + '\n\n'),
+                  TextSpan(text: 'EXPLAIN1'.tr() + '\n\n'),
+                  TextSpan(text: 'EXPLAIN2'.tr() + '\n\n'),
+                  TextSpan(text: 'EXPLAIN3'.tr() + '\n\n'),
+                  TextSpan(text: 'EXPLAIN4'.tr() + '\n\n'),
+                  TextSpan(text: 'EXPLAIN5'.tr() + '\n\n'),
+                  TextSpan(text: 'EXPLAIN6'.tr() + '\n\n'),
+                  TextSpan(text: 'EXPLAIN7'.tr() + '\n\n'),
+                  TextSpan(text: 'EXPLAIN8'.tr() + '\n\n'),
+                  TextSpan(text: 'EXPLAIN9'.tr() + '\n\n'),
+                  TextSpan(text: 'EXPLAIN10'.tr() + '\n\n'),
+                  TextSpan(text: 'EXPLAIN11'.tr() + '\n\n'),
+                  TextSpan(text: 'EXPLAIN12'.tr() + '\n\n'),
+                  TextSpan(text: 'EXPLAIN13'.tr() + '\n\n'),
+                  TextSpan(text: 'EXPLAIN14'.tr() + '\n\n'),
+                  TextSpan(text: 'EXPLAIN15'.tr() + '\n\n'),
+                ]),
           ),
-          // Text('The power of protest is to reveal truth in the face of unjust power through non-violent action.'),
+        ),
+      ),
+      // Text('The power of protest is to reveal truth in the face of unjust power through non-violent action.'),
     ]);
   }
 }
